452b29e652bf9032b4d422a14c4084dc56b8b87f
kim hanwoong <khw7096@gmail.com>
2017-02-08 10:02:04 +0900

    구조만 1차 푸쉬

---
+++ dipath.py
@@ -0,0 +1,21 @@
+#coding:utf8
+
+def getproject(path):
+	"""
+	경로에서 프로젝트를 가지고 온다.
+	"""
+	pass
+
+
+def getSeq(shotname):
+	"""
+	샷 네임에서 seq 이름을 가지고 온다.
+
+	"""
+	pass
+
+def getShot(shotstring):
+	"""
+	SS_0010_comp_v01, A0010_SS_0010_comp_v01 형태의 문자에서 샷문자를 반환한다.
+	"""
+	pass
