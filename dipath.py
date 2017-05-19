e4a12ce029810f874a5de37e9cd7e37a597ea188
kim hanwoong <khw7096@gmail.com>
2017-05-19 20:00:58 +0900

    get을 제거함

--- dipath.py
+++ dipath.py
@@ -2,7 +2,7 @@
 import re
 import os
 
-def GetProject(path):
+def Project(path):
 	"""
 	경로를 받아서 프로젝트 문자열과 에러값을 리턴한다.
 	만약 리턴할 프로젝트 문자열이 없으면 ""를 리턴한다.
@@ -17,14 +17,14 @@ def GetProject(path):
 		return backup[0], None
 	return "", "경로에서 프로젝트를 가지고 올 수 없습니다."
 
-def GetSeq(shotname):
+def Seq(shotname):
 	"""
 	샷 네임에서 seq 이름을 가지고 온다.
 
 	"""
 	pass
 
-def GetShot(shotstring):
+def Shot(shotstring):
 	"""
 	일반형태 SS_0010_comp_v01, 롤넘버가 붙은 형태 A0010_SS_0010_comp_v01 문자열에서 샷문자 SS_0010 을 반환한다.
 	"""
