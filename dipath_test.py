b6312430dcb55899edf376adf2b02a01d0c564ab
kim hanwoong <khw7096@gmail.com>
2017-05-22 15:20:33 +0900

    주석 추가

--- dipath_test.py
+++ dipath_test.py
@@ -21,12 +21,12 @@ class Test_dipath(unittest.TestCase):
 		self.assertEqual(Project("/lustre/INHouse/CentOS/bin"), ("","경로에서 프로젝트를 가지고 올 수 없습니다."))
 
 	def test_Shot(self):
-		self.assertEqual(Shot("A0000_SS_0010_comp_v01"), ("SS_0010",None))
-		self.assertEqual(Shot("A0000_SS_0010_v01"), ("SS_0010",None))
+		self.assertEqual(Shot("A0000_SS_0010_comp_v01"), ("SS_0010",None)) # 롤넘버가 존재하는 형태
+		self.assertEqual(Shot("A0000_SS_0010_v01"), ("SS_0010",None)) # 롤넘버가 존재하는 형태
 		self.assertEqual(Shot("SS_0010_v01"), ("SS_0010",None))
 		self.assertEqual(Shot("SS_0010"), ("SS_0010",None))
 		self.assertEqual(Shot("R1VFX_sh033_comp_v01"), ("R1VFX_sh033",None))
-		self.assertEqual(Shot("/show/TEMP/seq/R1VFX/R1VFX_sh033/comp/dev/R1VFX_sh033_comp_v01"), ("R1VFX_sh033",None)) # 상위경로를 믿는다.
+		self.assertEqual(Shot("/show/TEMP/seq/R1VFX/R1VFX_sh033/comp/dev/R1VFX_sh033_comp_v01"), ("R1VFX_sh033",None)) # 상위경로는 파이프라인툴로 제작된다. 하위보다는 상위경로를 더 신뢰하도록 한다.
 		self.assertEqual(Shot("/show/TEMP/product/out/confirm/170522/R1VFX_sh033_comp_v01"), ("R1VFX_sh033",None))
 		self.assertEqual(Shot("SS_0010_00_previz_v001.mov"), ("SS_0010",None))
 		self.assertEqual(Shot("SDF"), ("","샷이름을 추출할 수 없습니다."))
