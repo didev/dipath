04cc4da240f8d77250ff4adfd0c772a860647425
kim hanwoong <khw7096@gmail.com>
2017-05-22 15:17:54 +0900

    Shot 함수 추가

--- dipath_test.py
+++ dipath_test.py
@@ -20,5 +20,16 @@ class Test_dipath(unittest.TestCase):
 		self.assertEqual(Project("/backup/2016/TEMP/org_fin"), ("TEMP",None))
 		self.assertEqual(Project("/lustre/INHouse/CentOS/bin"), ("","경로에서 프로젝트를 가지고 올 수 없습니다."))
 
+	def test_Shot(self):
+		self.assertEqual(Shot("A0000_SS_0010_comp_v01"), ("SS_0010",None))
+		self.assertEqual(Shot("A0000_SS_0010_v01"), ("SS_0010",None))
+		self.assertEqual(Shot("SS_0010_v01"), ("SS_0010",None))
+		self.assertEqual(Shot("SS_0010"), ("SS_0010",None))
+		self.assertEqual(Shot("R1VFX_sh033_comp_v01"), ("R1VFX_sh033",None))
+		self.assertEqual(Shot("/show/TEMP/seq/R1VFX/R1VFX_sh033/comp/dev/R1VFX_sh033_comp_v01"), ("R1VFX_sh033",None)) # 상위경로를 믿는다.
+		self.assertEqual(Shot("/show/TEMP/product/out/confirm/170522/R1VFX_sh033_comp_v01"), ("R1VFX_sh033",None))
+		self.assertEqual(Shot("SS_0010_00_previz_v001.mov"), ("SS_0010",None))
+		self.assertEqual(Shot("SDF"), ("","샷이름을 추출할 수 없습니다."))
+
 if __name__ == "__main__":
 	unittest.main()
