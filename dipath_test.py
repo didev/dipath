4e61eb58077e3909d90d6608f22fc24df933fcd2
kim hanwoong <d10191@idea.co.kr>
2017-12-22 18:19:08 +0900

    빠진 타입추가

--- dipath_test.py
+++ dipath_test.py
@@ -16,6 +16,8 @@ class Test_dipath(unittest.TestCase):
 		self.assertEqual(Project("\\\\10.0.200.101\\lustre3\\show_TEMP\\seq"), ("TEMP",None))
 		self.assertEqual(Project("//10.0.200.101/lustr2/show_TEMP/seq"), ("TEMP",None))
 		self.assertEqual(Project("//10.0.200.101/lustre3/show_TEMP/seq"), ("TEMP",None))
+		self.assertEqual(Project("//10.0.200.100/show_TEMP/seq"), ("TEMP",None))
+		self.assertEqual(Project("\\\\10.0.200.100\\show_TEMP\\seq"), ("TEMP",None))
 		self.assertEqual(Project("/fxdata/cache/show/TEMP/seq"), ("TEMP",None))
 		self.assertEqual(Project("/backup/2016/TEMP/org_fin"), ("TEMP",None))
 		self.assertEqual(Project("/lustre/INHouse/CentOS/bin"), ("","경로에서 프로젝트를 가지고 올 수 없습니다."))
